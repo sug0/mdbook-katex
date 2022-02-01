34f85e6f21bebc9a3a617d0b5158ce9001dc3a8e
rogeryoungh <rogeryoungh@foxmail.com>
2022-02-01 19:37:21 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,8 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::{Path, PathBuf};
-use std::vec::Vec;
-
-use lazy_static::lazy_static;
-use regex::Regex;
-use serde_derive::{Deserialize, Serialize};
-use toml;
-
 use mdbook::book::{Book, BookItem};
 use mdbook::errors::Error;
 use mdbook::errors::Result;
 use mdbook::preprocess::{Preprocessor, PreprocessorContext};
 use mdbook::renderer::{RenderContext, Renderer};
-use mdbook::utils::fs::path_to_root;
-
-#[derive(Deserialize, Serialize)]
-#[serde(default, rename_all = "kebab-case")]
-pub struct KatexConfig {
-    // options for the katex-rust crate
-    pub leqno: bool,
-    pub fleqn: bool,
-    pub throw_on_error: bool,
-    pub error_color: String,
-    pub min_rule_thickness: f64,
-    pub max_size: f64,
-    pub max_expand: i32,
-    pub trust: bool,
-    // other options
-    pub static_css: bool,
-    pub macros: Option<String>,
-}
-
-impl Default for KatexConfig {
-    fn default() -> KatexConfig {
-        KatexConfig {
-            // default options for the katex-rust crate
-            // uses defaults specified in: https://katex.org/docs/options.html
-            leqno: false,
-            fleqn: false,
-            throw_on_error: true,
-            error_color: String::from("#cc0000"),
-            min_rule_thickness: -1.0,
-            max_size: f64::INFINITY,
-            max_expand: 1000,
-            trust: false,
-            // other options
-            static_css: false,
-            macros: None,
-        }
-    }
-}
 
 // ensures that both the preprocessor and renderers are enabled
 // in the `book.toml`; the renderer forces mdbook to separate all
@@ -61,12 +11,6 @@ impl Default for KatexConfig {
 fn enforce_config(cfg: &mdbook::Config) {
     if cfg.get("preprocessor.katex").is_none() {
         panic!("Missing `[preprocessor.katex]` directive in `book.toml`!");
-    }
-    if cfg.get("output.katex").is_none() {
-        panic!("Missing `[output.katex]` directive in `book.toml`!");
-    }
-    if cfg.get("output.html").is_none() {
-        panic!("The katex preprocessor is only compatible with the html renderer!");
     }
 }
 
@@ -95,21 +39,9 @@ impl Preprocessor for KatexProcessor {
         // enforce config requirements
         enforce_config(&ctx.config);
         // parse TOML config
-        let cfg = get_config(&ctx.config)?;
-        let (inline_opts, display_opts) = self.build_opts(&ctx, &cfg);
-        // get stylesheet header
-        let stylesheet_header_generator =
-            katex_header(&ctx.root, &ctx.config.build.build_dir, &cfg)?;
         book.for_each_mut(|item| {
             if let BookItem::Chapter(chapter) = item {
-                let stylesheet_header =
-                    stylesheet_header_generator(path_to_root(chapter.path.clone().unwrap()));
-                chapter.content = self.process_chapter(
-                    &chapter.content,
-                    &inline_opts,
-                    &display_opts,
-                    &stylesheet_header,
-                )
+                chapter.content = self.process_chapter(&chapter.content)
             }
         });
         Ok(book)
@@ -121,248 +53,44 @@ impl Preprocessor for KatexProcessor {
 }
 
 impl KatexProcessor {
-    fn build_opts(
-        &self,
-        ctx: &PreprocessorContext,
-        cfg: &KatexConfig,
-    ) -> (katex::Opts, katex::Opts) {
-        let configure_katex_opts = || -> katex::OptsBuilder {
-            katex::Opts::builder()
-                .leqno(cfg.leqno)
-                .fleqn(cfg.fleqn)
-                .throw_on_error(cfg.throw_on_error)
-                .error_color(cfg.error_color.clone())
-                .min_rule_thickness(cfg.min_rule_thickness)
-                .max_size(cfg.max_size)
-                .max_expand(cfg.max_expand)
-                .trust(cfg.trust)
-                .clone()
-        };
-        // load macros as a HashMap
-        let macros = Self::load_macros(&ctx, &cfg.macros);
-        // inline rendering options
-        let inline_opts = configure_katex_opts()
-            .display_mode(false)
-            .output_type(katex::OutputType::Html)
-            .macros(macros.clone())
-            .build()
-            .unwrap();
-        // display rendering options
-        let display_opts = configure_katex_opts()
-            .display_mode(true)
-            .output_type(katex::OutputType::Html)
-            .macros(macros)
-            .build()
-            .unwrap();
-        (inline_opts, display_opts)
-    }
-
-    fn load_macros(
-        ctx: &PreprocessorContext,
-        macros_path: &Option<String>,
-    ) -> HashMap<String, String> {
-        // load macros as a HashMap
-        let mut map = HashMap::new();
-        if let Some(path) = get_macro_path(&ctx.root, &macros_path) {
-            let macro_str = load_as_string(&path);
-            for couple in macro_str.split("\n") {
-                // only consider lines starting with a backslash
-                if let Some('\\') = couple.chars().next() {
-                    let couple: Vec<&str> = couple.splitn(2, ":").collect();
-                    map.insert(String::from(couple[0]), String::from(couple[1]));
-                }
-            }
-        }
-        map
-    }
-
     // render Katex equations in HTML, and add the Katex CSS
-    fn process_chapter(
-        &self,
-        raw_content: &str,
-        inline_opts: &katex::Opts,
-        display_opts: &katex::Opts,
-        stylesheet_header: &String,
-    ) -> String {
-        let mut rendered_content = stylesheet_header.clone();
-        // render display equations
-        let content = Self::render_between_delimiters(&raw_content, "$$", display_opts, false);
-        // render inline equations
-        let content = Self::render_between_delimiters(&content, "$", inline_opts, true);
-        rendered_content.push_str(&content);
-        rendered_content
+    fn process_chapter(&self, raw_content: &str) -> String {
+        return Self::render(&raw_content);
     }
 
-    // render equations between given delimiters, with specified options
-    fn render_between_delimiters(
-        raw_content: &str,
-        delimiters: &str,
-        opts: &katex::Opts,
-        escape_backslash: bool,
-    ) -> String {
-        let mut rendered_content = String::new();
-        let mut inside_delimiters = false;
-        for item in Self::split(&raw_content, &delimiters, escape_backslash) {
-            if inside_delimiters {
-                // try to render equation
-                if let Ok(rendered) = katex::render_with_opts(&item, opts) {
-                    rendered_content.push_str(&rendered)
-                // if rendering fails, keep the unrendered equation
+    fn render(string: &str) -> String {
+        let mut result = String::new();
+
+        let mut past_char = '\0';
+        let mut in_codeblocks = false;
+        let mut in_math = false;
+        for c in string.chars() {
+            // println!("{}", temp_string);
+            if '`' == c {
+                if '`' == past_char {
+                    // do nothing: skip muti-backtick
                 } else {
-                    rendered_content.push_str(&item)
+                    in_codeblocks = !in_codeblocks;
                 }
-            // outside delimiters
-            } else {
-                rendered_content.push_str(&item)
-            }
-            inside_delimiters = !inside_delimiters;
-        }
-        rendered_content
-    }
-
-    fn split(string: &str, separator: &str, escape_backslash: bool) -> Vec<String> {
-        let mut result = Vec::new();
-        let mut splits = string.split(separator);
-        let mut current_split = splits.next();
-        // iterate over splits
-        while let Some(substring) = current_split {
-            let mut result_split = String::from(substring);
-            if escape_backslash {
-                // while the current split ends with a backslash
-                while let Some('\\') = current_split.unwrap().chars().last() {
-                    // removes the backslash, add the separator back, and add the next split
-                    result_split.pop();
-                    result_split.push_str(separator);
-                    current_split = splits.next();
-                    if let Some(split) = current_split {
-                        result_split.push_str(split);
-                    }
+            } else if in_codeblocks {
+                // do nothing: skip codeblocks
+            } else if '$' == c {
+                if '$' != past_char {
+                    in_math = !in_math;
+                }
+            } else if in_math {
+                if '_' == c {
+                    result.push('\\');
+                } else if '*' == c {
+                    result.push('\\');
+                } else if '\\' == c {
+                    result.push('\\');
                 }
             }
-            result.push(result_split);
-            current_split = splits.next()
+            result.push(c);
+            past_char = c;
         }
-        result
-    }
-}
-
-pub fn get_macro_path(root: &PathBuf, macros_path: &Option<String>) -> Option<PathBuf> {
-    match macros_path {
-        Some(path) => Some(root.join(PathBuf::from(path))),
-        _ => None,
-    }
-}
-
-pub fn get_config(book_cfg: &mdbook::Config) -> Result<KatexConfig, toml::de::Error> {
-    let cfg = match book_cfg.get("preprocessor.katex") {
-        Some(raw) => raw.clone().try_into(),
-        None => Ok(KatexConfig::default()),
-    };
-    cfg.or_else(|_| Ok(KatexConfig::default()))
-}
-
-pub fn load_as_string(path: &Path) -> String {
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-
-    let mut string = String::new();
-    match file.read_to_string(&mut string) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => (),
-    };
-    string
-}
-
-fn katex_header(
-    build_root: &PathBuf,
-    build_dir: &PathBuf,
-    cfg: &KatexConfig,
-) -> Result<Box<dyn Fn(String) -> String>, Error> {
-    // constants
-    let cdn_root = "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/";
-    let stylesheet_url = format!("{}katex.min.css", cdn_root);
-    let integrity = "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X";
-
-    if cfg.static_css {
-        // create katex resource directory
-        let mut katex_dir_path = build_root.join(build_dir);
-        katex_dir_path.push("html/katex");
-        if !katex_dir_path.exists() {
-            std::fs::create_dir_all(katex_dir_path.as_path())?;
-        }
-
-        // download or fetch stylesheet content
-        let mut stylesheet_path = katex_dir_path.clone();
-        stylesheet_path.push("katex.min.css");
-
-        let mut stylesheet: String;
-        if !stylesheet_path.exists() {
-            // download stylesheet content
-            let stylesheet_response = reqwest::blocking::get(stylesheet_url)?;
-            stylesheet = String::from(std::str::from_utf8(&stylesheet_response.bytes()?)?);
-            // create stylesheet file and populate it with the content
-            let mut stylesheet_file = File::create(stylesheet_path.as_path())?;
-            stylesheet_file.write_all(stylesheet.as_str().as_bytes())?;
-        } else {
-            // read stylesheet content from disk
-            stylesheet = String::new();
-            let mut stylesheet_file = File::open(stylesheet_path.as_path())?;
-            stylesheet_file.read_to_string(&mut stylesheet)?;
-        }
-
-        // download all resources from stylesheet
-        lazy_static! {
-            static ref URL_PATTERN: Regex = Regex::new(r"(url)\s*[(]([^()]*)[)]").unwrap();
-            static ref REL_PATTERN: Regex = Regex::new(r"[.][.][/\\]|[.][/\\]").unwrap();
-        }
-        let mut resources: HashSet<String> = HashSet::new();
-        for capture in URL_PATTERN.captures_iter(&stylesheet) {
-            let resource_name = String::from(&capture[2]);
-            // sanitize resource path
-            let mut resource_path = katex_dir_path.clone();
-            resource_path.push(&resource_name);
-            resource_path = PathBuf::from(String::from(
-                REL_PATTERN.replace_all(resource_path.to_str().unwrap(), ""),
-            ));
-            // create resource path and populate content
-            if !resource_path.as_path().exists() {
-                // don't download resources if they already exist
-                if resources.insert(String::from(&capture[2])) {
-                    // create all leading directories
-                    let mut resource_parent_dir = resource_path.clone();
-                    resource_parent_dir.pop();
-                    std::fs::create_dir_all(resource_parent_dir.as_path())?;
-                    // create resource file
-                    let mut resource_file = File::create(resource_path)?;
-                    // download content
-                    let resource_url = format!("{}{}", cdn_root, &resource_name);
-                    let resource_response = reqwest::blocking::get(&resource_url)?;
-                    // populate file with content
-                    resource_file.write_all(&resource_response.bytes()?)?;
-                }
-            }
-        }
-
-        // return closure capable of generating relative paths to the katex
-        // resources
-        Ok(Box::new(move |path: String| -> String {
-            // generate a style element with a relative local path to
-            // the katex stylesheet
-            String::from(format!(
-                "<link rel=\"stylesheet\" href=\"{}katex/katex.min.css\">\n\n",
-                path,
-            ))
-        }))
-    } else {
-        let stylesheet = String::from(format!(
-            "<link rel=\"stylesheet\" href=\"{}\" integrity=\"{}\" crossorigin=\"anonymous\">\n\n",
-            stylesheet_url, integrity,
-        ));
-        Ok(Box::new(move |_: String| -> String { stylesheet.clone() }))
+        return result;
     }
 }
 
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,4 @@
 use super::*;
-use std::str::FromStr;
 
 #[test]
 fn test_name() {
@@ -15,206 +14,41 @@ fn test_support_html() {
     assert!(!preprocessor.supports_renderer("other_renderer"))
 }
 
-fn mock_build_opts(
-    macros: HashMap<String, String>,
-    cfg: &KatexConfig,
-) -> (katex::Opts, katex::Opts) {
-    let configure_katex_opts = || -> katex::OptsBuilder {
-        katex::Opts::builder()
-            .leqno(cfg.leqno)
-            .fleqn(cfg.fleqn)
-            .throw_on_error(cfg.throw_on_error)
-            .error_color(cfg.error_color.clone())
-            .min_rule_thickness(cfg.min_rule_thickness)
-            .max_size(cfg.max_size)
-            .max_expand(cfg.max_expand)
-            .trust(cfg.trust)
-            .clone()
-    };
-    let inline_opts = configure_katex_opts()
-        .display_mode(false)
-        .output_type(katex::OutputType::Html)
-        .macros(macros.clone())
-        .build()
-        .unwrap();
-    let display_opts = configure_katex_opts()
-        .display_mode(true)
-        .output_type(katex::OutputType::Html)
-        .macros(macros)
-        .build()
-        .unwrap();
-    (inline_opts, display_opts)
-}
-
 #[test]
 fn test_rendering_without_math() {
     let preprocessor = KatexProcessor;
-    let macros = HashMap::new();
-    let mut cfg = KatexConfig::default();
-    cfg.static_css = false;
-    let (inline_opts, display_opts) = mock_build_opts(macros, &cfg);
     let raw_content = r"Some text, and more text.";
-    let build_root = PathBuf::new();
-    let build_dir = PathBuf::from("book");
-    let stylesheet_header_generator = katex_header(&build_root, &build_dir, &cfg).unwrap();
-    let stylesheet_header = stylesheet_header_generator("".to_string());
-    let mut expected_output = String::from("");
-    expected_output.push_str(&stylesheet_header);
-    expected_output.push_str(raw_content);
-    let rendered_content = preprocessor.process_chapter(
-        &raw_content,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
-    );
+    let expected_output = String::from("Some text, and more text.");
+    let rendered_content = preprocessor.process_chapter(&raw_content);
     debug_assert_eq!(expected_output, rendered_content);
 }
 
 #[test]
-fn test_dollar_escaping() {
+fn test_rendering_with_math() {
     let preprocessor = KatexProcessor;
-    let macros = HashMap::new();
-    let mut cfg = KatexConfig::default();
-    cfg.static_css = false;
-    let (inline_opts, display_opts) = mock_build_opts(macros, &cfg);
-    let raw_content = r"Some text, \$\$ and more text.";
-    let build_root = PathBuf::new();
-    let build_dir = PathBuf::from("book");
-    let stylesheet_header_generator = katex_header(&build_root, &build_dir, &cfg).unwrap();
-    let stylesheet_header = stylesheet_header_generator("".to_string());
-    let mut expected_output = String::from("");
-    expected_output.push_str(&stylesheet_header);
-    expected_output.push_str(r"Some text, $$ and more text.");
-    let rendered_content = preprocessor.process_chapter(
-        &raw_content,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
+    let raw_content = r"A simple fomula, $\sum_{n=1}^\infty \frac{1}{n^2} = \frac{\pi^2}{6}$.";
+    let expected_output = String::from(
+        r"A simple fomula, $\\sum\_{n=1}^\\infty \\frac{1}{n^2} = \\frac{\\pi^2}{6}$.",
     );
+    let rendered_content = preprocessor.process_chapter(&raw_content);
     debug_assert_eq!(expected_output, rendered_content);
 }
 
 #[test]
-fn test_inline_rendering() {
+fn test_rendering_underscore() {
     let preprocessor = KatexProcessor;
-    let macros = HashMap::new();
-    let mut cfg = KatexConfig::default();
-    cfg.static_css = false;
-    let (inline_opts, display_opts) = mock_build_opts(macros, &cfg);
-    let raw_content = r"Some text, $\nabla f(x) \in \mathbb{R}^n$, and more text.";
-    let build_root = PathBuf::new();
-    let build_dir = PathBuf::from("book");
-    let stylesheet_header_generator = katex_header(&build_root, &build_dir, &cfg).unwrap();
-    let stylesheet_header = stylesheet_header_generator("".to_string());
-    let mut expected_output = String::from("");
-    expected_output.push_str(&stylesheet_header);
-    expected_output.push_str("Some text, <span class=\"katex\"><span class=\"katex-html\" aria-hidden=\"true\"><span class=\"base\"><span class=\"strut\" style=\"height:1em;vertical-align:-0.25em;\"></span><span class=\"mord\">∇</span><span class=\"mord mathnormal\" style=\"margin-right:0.10764em;\">f</span><span class=\"mopen\">(</span><span class=\"mord mathnormal\">x</span><span class=\"mclose\">)</span><span class=\"mspace\" style=\"margin-right:0.2777777777777778em;\"></span><span class=\"mrel\">∈</span><span class=\"mspace\" style=\"margin-right:0.2777777777777778em;\"></span></span><span class=\"base\"><span class=\"strut\" style=\"height:0.68889em;vertical-align:0em;\"></span><span class=\"mord\"><span class=\"mord mathbb\">R</span><span class=\"msupsub\"><span class=\"vlist-t\"><span class=\"vlist-r\"><span class=\"vlist\" style=\"height:0.664392em;\"><span style=\"top:-3.063em;margin-right:0.05em;\"><span class=\"pstrut\" style=\"height:2.7em;\"></span><span class=\"sizing reset-size6 size3 mtight\"><span class=\"mord mathnormal mtight\">n</span></span></span></span></span></span></span></span></span></span></span>, and more text.");
-    let rendered_content = preprocessor.process_chapter(
-        &raw_content,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
-    );
+    let raw_content = r"A simple `f_f_f`, f_f_f, f`f$f_$f_` fomula, $\sum_{n=1}^\infty\\$.";
+    let expected_output =
+        String::from(r"A simple `f_f_f`, f_f_f, f`f$f_$f_` fomula, $\\sum\_{n=1}^\\infty\\\\$.");
+    let rendered_content = preprocessor.process_chapter(&raw_content);
     debug_assert_eq!(expected_output, rendered_content);
 }
 
 #[test]
-fn test_display_rendering() {
+fn test_rendering_braces() {
     let preprocessor = KatexProcessor;
-    let macros = HashMap::new();
-    let mut cfg = KatexConfig::default();
-    cfg.static_css = false;
-    let (inline_opts, display_opts) = mock_build_opts(macros, &cfg);
-    let raw_content = r"Some text, $\nabla f(x) \in \mathbb{R}^n$, and more text.";
-    let build_root = PathBuf::new();
-    let build_dir = PathBuf::from("book");
-    let stylesheet_header_generator = katex_header(&build_root, &build_dir, &cfg).unwrap();
-    let stylesheet_header = stylesheet_header_generator("".to_string());
-    let mut expected_output = String::from("");
-    expected_output.push_str(&stylesheet_header);
-    expected_output.push_str("Some text, <span class=\"katex\"><span class=\"katex-html\" aria-hidden=\"true\"><span class=\"base\"><span class=\"strut\" style=\"height:1em;vertical-align:-0.25em;\"></span><span class=\"mord\">∇</span><span class=\"mord mathnormal\" style=\"margin-right:0.10764em;\">f</span><span class=\"mopen\">(</span><span class=\"mord mathnormal\">x</span><span class=\"mclose\">)</span><span class=\"mspace\" style=\"margin-right:0.2777777777777778em;\"></span><span class=\"mrel\">∈</span><span class=\"mspace\" style=\"margin-right:0.2777777777777778em;\"></span></span><span class=\"base\"><span class=\"strut\" style=\"height:0.68889em;vertical-align:0em;\"></span><span class=\"mord\"><span class=\"mord mathbb\">R</span><span class=\"msupsub\"><span class=\"vlist-t\"><span class=\"vlist-r\"><span class=\"vlist\" style=\"height:0.664392em;\"><span style=\"top:-3.063em;margin-right:0.05em;\"><span class=\"pstrut\" style=\"height:2.7em;\"></span><span class=\"sizing reset-size6 size3 mtight\"><span class=\"mord mathnormal mtight\">n</span></span></span></span></span></span></span></span></span></span></span>, and more text.");
-    let rendered_content = preprocessor.process_chapter(
-        &raw_content,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
-    );
+    let raw_content = r"define $\{a_i\} = \{ a \mid a * b * c \}$.";
+    let expected_output = String::from(r"define $\\{a\_i\\} = \\{ a \\mid a \* b \* c \\}$.");
+    let rendered_content = preprocessor.process_chapter(&raw_content);
     debug_assert_eq!(expected_output, rendered_content);
-}
-
-#[test]
-fn test_macros_without_argument() {
-    let preprocessor = KatexProcessor;
-    let mut macros = HashMap::new();
-    macros.insert(String::from(r"\grad"), String::from(r"\nabla"));
-    let mut cfg = KatexConfig::default();
-    cfg.static_css = false;
-    let (inline_opts, display_opts) = mock_build_opts(macros, &cfg);
-    let raw_content_no_macro = r"Some text, $\nabla f(x) \in \mathbb{R}^n$, and more text.";
-    let raw_content_macro = r"Some text, $\grad f(x) \in \mathbb{R}^n$, and more text.";
-    let build_root = PathBuf::new();
-    let build_dir = PathBuf::from("book");
-    let stylesheet_header_generator = katex_header(&build_root, &build_dir, &cfg).unwrap();
-    let stylesheet_header = stylesheet_header_generator("".to_string());
-    let rendered_content_macro = preprocessor.process_chapter(
-        &raw_content_macro,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
-    );
-    let rendered_content_no_macro = preprocessor.process_chapter(
-        &raw_content_no_macro,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
-    );
-    debug_assert_eq!(rendered_content_macro, rendered_content_no_macro);
-}
-
-#[test]
-fn test_macros_with_argument() {
-    let preprocessor = KatexProcessor;
-    let mut macros = HashMap::new();
-    macros.insert(String::from(r"\R"), String::from(r"\mathbb{R}^#1"));
-    let mut cfg = KatexConfig::default();
-    cfg.static_css = false;
-    let (inline_opts, display_opts) = mock_build_opts(macros, &cfg);
-    let raw_content_no_macro = r"Some text, $\nabla f(x) \in \mathbb{R}^1$, and more text.";
-    let raw_content_macro = r"Some text, $\nabla f(x) \in \R{1}$, and more text.";
-    let build_root = PathBuf::new();
-    let build_dir = PathBuf::from("book");
-    let stylesheet_header_generator = katex_header(&build_root, &build_dir, &cfg).unwrap();
-    let stylesheet_header = stylesheet_header_generator("".to_string());
-    let rendered_content_macro = preprocessor.process_chapter(
-        &raw_content_macro,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
-    );
-    let rendered_content_no_macro = preprocessor.process_chapter(
-        &raw_content_no_macro,
-        &inline_opts,
-        &display_opts,
-        &stylesheet_header,
-    );
-    debug_assert_eq!(rendered_content_macro, rendered_content_no_macro);
-}
-
-#[test]
-fn test_macro_file_loading() {
-    let cfg_str = r#"
-    [book]
-    src = "src"
-
-    [preprocessor.katex]
-    macros = "macros.txt"
-    "#;
-
-    let book_cfg = mdbook::config::Config::from_str(cfg_str).unwrap();
-    let cfg = get_config(&book_cfg).unwrap();
-
-    debug_assert_eq!(
-        get_macro_path(&PathBuf::from("book"), &cfg.macros),
-        Some(PathBuf::from("book/macros.txt")) // We supply a root, just like the preproccessor context does
-    );
 }
